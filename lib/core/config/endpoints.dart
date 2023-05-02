c3aafc034a5637d0333205df3395280d2482b544
Victor <vctudo.27@gmail.com>
2023-05-01 21:49:30 -0300

    🎉 Hello world!

---
+++ lib/core/config/endpoints.dart
@@ -0,0 +1,5 @@
+const String baseUrl = "https://sys-dev.searchandstay.com/api";
+const String admin = "/admin";
+const String houseRuleEndpoint = "/house_rules/";
+
+const String adminHouseRuleEndpoint = baseUrl + admin + houseRuleEndpoint;
