fbbb30be07ad6ce3b51b6b0d83460d2cb3220d5d
Victor <vctudo.27@gmail.com>
2023-05-01 21:45:48 -0300

    🎉 Hello world!

---
+++ android/app/src/main/kotlin/com/searchandstay/test/search_and_stay_test/core/methodchannel/HttpDeleteChannel.kt
@@ -0,0 +1,11 @@
+package com.searchandstay.test.search_and_stay_test.core.methodchannel
+
+import com.searchandstay.test.search_and_stay_test.core.methodchannel.core.HttpChannel
+import io.flutter.plugin.common.MethodChannel
+import retrofit2.Retrofit
+
+data class HttpDeleteChannel(
+    override var channel: MethodChannel,
+    override val retrofit: Retrofit.Builder,
+) :
+    HttpChannel
