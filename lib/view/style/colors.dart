6da7a1cdd61cda374d32543216c255cc3f4afe77
Victor <vctudo.27@gmail.com>
2023-05-01 21:50:59 -0300

    🎉 Hello world!

---
+++ lib/view/style/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const backgroundColor = Color(0xFF2D2C2C);
+  static const primary = Color(0xFFFFFFFF);
+  static const onPrimary = Color(0xFF1F1E1E);
+  static const secondary = Color(0xFF0085FF);
+  static const onSecondary = Color(0xFF403D39);
+}
