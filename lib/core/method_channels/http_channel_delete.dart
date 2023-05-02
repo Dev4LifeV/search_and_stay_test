72107fa8caeb289c4f76e7409af13474af0310d6
Victor <vctudo.27@gmail.com>
2023-05-01 21:49:55 -0300

    🎉 Hello world!

---
+++ lib/core/method_channels/http_channel_delete.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/services.dart';
+import 'package:search_and_stay_test/core/method_channels/channel.dart';
+import 'package:search_and_stay_test/core/method_channels/channels.dart';
+import 'package:search_and_stay_test/core/method_channels/method_result.dart';
+
+class HttpChannelDelete extends Channel {
+  @override
+  final MethodChannel channel = const MethodChannel(channelHttpDelete);
+
+  Future<MethodResult> delete(String url, {String? token, dynamic args}) async {
+    try {
+      return invoke(
+        [
+          url,
+          token,
+          args,
+        ],
+      );
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
