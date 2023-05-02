03fe8d89a455e63a346e9304c0c3ac36f2b65880
Victor <vctudo.27@gmail.com>
2023-05-01 21:48:44 -0300

    🎉 Hello world!

---
+++ ios/Runner/Channels.swift
@@ -0,0 +1,18 @@
+//
+//  Channels.swift
+//  Runner
+//
+//  Created by Victor Proppe on 29/04/23.
+//
+
+import Foundation
+
+class Channels {
+    public static let channelMain = "com.searchandstay.test.search_and_stay_test"
+    
+    
+    public static let channelHttpGet = "\(channelMain)/http/get";
+    public static let channelHttpPost = "\(channelMain)/http/post";
+    public static let channelHttpPut = "\(channelMain)/http/put";
+    public static let channelHttpDelete = "\(channelMain)/http/delete";
+}
