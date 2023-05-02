7e25367bee335386e85c10dc52d3ef3432898b24
Victor <vctudo.27@gmail.com>
2023-05-01 21:49:53 -0300

    🎉 Hello world!

---
+++ lib/core/method_channels/channels.dart
@@ -0,0 +1,6 @@
+const String channelMain = "com.searchandstay.test.search_and_stay_test";
+
+const String channelHttpGet = "$channelMain/http/get";
+const String channelHttpPost = "$channelMain/http/post";
+const String channelHttpPut = "$channelMain/http/put";
+const String channelHttpDelete = "$channelMain/http/delete";
