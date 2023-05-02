371ee363dad420146fd04ec36788f3c2b0e88a75
Victor <vctudo.27@gmail.com>
2023-05-01 21:45:53 -0300

    🎉 Hello world!

---
+++ android/app/src/main/kotlin/com/searchandstay/test/search_and_stay_test/core/methodchannel/HttpPostChannel.kt
@@ -0,0 +1,12 @@
+package com.searchandstay.test.search_and_stay_test.core.methodchannel
+
+import com.searchandstay.test.search_and_stay_test.core.methodchannel.core.HttpChannel
+import io.flutter.plugin.common.MethodChannel
+import retrofit2.Retrofit
+
+
+data class HttpPostChannel(
+    override var channel: MethodChannel,
+    override val retrofit: Retrofit.Builder,
+) :
+    HttpChannel
