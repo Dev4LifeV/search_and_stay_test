b28360eb7e4fa27505c4f94752960ab2972e0f3e
Victor <vctudo.27@gmail.com>
2023-05-01 21:45:37 -0300

    🎉 Hello world!

---
+++ android/app/src/main/kotlin/com/searchandstay/test/search_and_stay_test/core/methodchannel/core/Channels.kt
@@ -0,0 +1,9 @@
+package com.searchandstay.test.search_and_stay_test.core.methodchannel.core
+
+object Channels {
+    private const val channelMain = "com.searchandstay.test.search_and_stay_test"
+    const val channelHttpGet = "$channelMain/http/get"
+    const val channelHttpPost = "$channelMain/http/post"
+    const val channelHttpPut = "$channelMain/http/put"
+    const val channelHttpDelete = "$channelMain/http/delete"
+}
